4cb4f8259a565d1c3c644e5b42b7099cba223fe9
csomtoo <chukwurahb@gmail.com>
2022-02-04 17:43:12 -0500

    Added functionality for command line

--- db/databaseOperations.js
+++ db/databaseOperations.js
@@ -1,103 +1,104 @@
 const yahooFinanceDataV10 = require('../financeData/yahooFinanceDataV10');
 const { downloadCSV } = require('./mongodb');
 
-
 async function updateDb(arr, db, portfolio) {
-  console.log('This is the arr updatedb', arr);
-  let updatePromise;
-  for (let x of arr) {
-    updatePromise = db.collection(portfolio).updateOne(
-      {
-        ticker: x,
-      },
-      {
-        $set: await getDataPromise(x),
-      }
-    );
-    updatePromise
-      .then((result) => {
-        // console.log(result);
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-  }
-  downloadCSV(portfolio)
+	console.log(portfolio);
+	let updatePromise;
+	for (let x of arr) {
+		updatePromise = db.collection(portfolio).updateOne(
+			{
+				ticker: x,
+			},
+			{
+				$set: await getDataPromise(x),
+			}
+		);
+		updatePromise
+			.then((result) => {})
+			.catch((err) => {
+				downloadCSV(portfolio);
+				// console.log(err);
+			});
+	}
+
+	downloadCSV(portfolio);
 }
 
 async function buildAndPopulateDatabase(arr, db, portfolio) {
-  console.log('This is the arr build and populate', arr);
+	console.log(portfolio);
+	console.log('This is the arr build and populate', arr);
 
-  for (let x of arr) {
-    db.collection(portfolio).insertOne(
-      await getDataPromise(x),
-      (err, result) => {
-        if (err) {
-          return console.log(err);
-        }
-        // console.log(result.ops);
-      }
-    );
-  }
-  downloadCSV(portfolio)
+	for (let x of arr) {
+		db.collection(portfolio).insertOne(
+			await getDataPromise(x),
+			(err, result) => {
+				if (err) {
+					return console.log(err);
+					downloadCSV(portfolio);
+				}
+				// console.log(result.ops);
+			}
+		);
+	}
+	downloadCSV(portfolio);
 }
 
 function getDataPromise(ticker) {
-  return new Promise((resolve) => {
-    getData(ticker, (finalObject) => {
-      resolve(finalObject);
-    });
-  });
+	return new Promise((resolve) => {
+		getData(ticker, (finalObject) => {
+			resolve(finalObject);
+		});
+	});
 }
 
 function getData(ticker, callback) {
-  let data0, firstObject, secondObject, finalObject;
-  // let combinedarr = [];
-  let data1 = { ticker };
-  yahooFinanceDataV10.financialData(ticker, (err, data) => {
-    if (!err) {
-      data0 = Object.assign(data1, data);
-      yahooFinanceDataV10.price(ticker, function (err, data) {
-        if (!err) {
-          firstObject = Object.assign(data0, data);
-          yahooFinanceDataV10.summaryDetail(ticker, (err, data) => {
-            if (!err) {
-              secondObject = Object.assign(firstObject, data);
-              yahooFinanceDataV10.defaultKeyStatistics(ticker, (err, data) => {
-                if (!err) {
-                  finalObject = Object.assign(secondObject, data);
-                  cleanData(finalObject);
-                  callback(finalObject);
+	let data0, firstObject, secondObject, finalObject;
+	// let combinedarr = [];
+	let data1 = { ticker };
+	yahooFinanceDataV10.financialData(ticker, (err, data) => {
+		if (!err) {
+			data0 = Object.assign(data1, data);
+			yahooFinanceDataV10.price(ticker, function (err, data) {
+				if (!err) {
+					firstObject = Object.assign(data0, data);
+					yahooFinanceDataV10.summaryDetail(ticker, (err, data) => {
+						if (!err) {
+							secondObject = Object.assign(firstObject, data);
+							yahooFinanceDataV10.defaultKeyStatistics(ticker, (err, data) => {
+								if (!err) {
+									finalObject = Object.assign(secondObject, data);
+									cleanData(finalObject);
+									callback(finalObject);
 
-                  // combinedarr.push(finalObject);
-                } else {
-                  console.log(err);
-                }
-              });
-            } else {
-              console.log(err);
-            }
-          });
-        } else {
-          console.log(err);
-        }
-      });
-    } else {
-      console.log(err);
-    }
-  });
+									// combinedarr.push(finalObject);
+								} else {
+									console.log(err);
+								}
+							});
+						} else {
+							console.log(err);
+						}
+					});
+				} else {
+					console.log(err);
+				}
+			});
+		} else {
+			console.log(err);
+		}
+	});
 }
 
 function cleanData(x) {
-  for (let r in x) {
-    if (typeof x[r] === 'object') {
-      for (let q in x[r]) {
-        if (q === 'raw') {
-          x[r] = x[r][q];
-        }
-      }
-    }
-  }
+	for (let r in x) {
+		if (typeof x[r] === 'object') {
+			for (let q in x[r]) {
+				if (q === 'raw') {
+					x[r] = x[r][q];
+				}
+			}
+		}
+	}
 }
 
 module.exports = { updateDb, buildAndPopulateDatabase };
--- index.js
+++ index.js
@@ -4,11 +4,19 @@ const multer = require('multer');
 const fs = require('fs');
 const path = require('path');
 const db = require('./db/mongodb');
+var FormData = require('form-data');
+var request = require('request');
+
+
 const MongoClient = mongodb.MongoClient;
+const axios = require('axios');
+
 const port = process.env.PORT;
+const add = process.env.ADD;
+
 const {
-  updateDb,
-  buildAndPopulateDatabase,
+	updateDb,
+	buildAndPopulateDatabase,
 } = require('./db/databaseOperations');
 const { rejects } = require('assert');
 const { callbackify } = require('util');
@@ -16,12 +24,12 @@ const app = express();
 const publicDirectoryPath = path.join(__dirname, '/public');
 
 const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, './uploads/');
-  },
-  filename: function (req, file, cb) {
-    cb(null, file.originalname);
-  },
+	destination: function (req, file, cb) {
+		cb(null, './uploads/');
+	},
+	filename: function (req, file, cb) {
+		cb(null, file.originalname);
+	},
 });
 
 const upload = multer({ storage: storage });
@@ -31,86 +39,116 @@ app.set('views', path.join(__dirname, '/views'));
 app.set('view engine', 'ejs');
 
 app.get('/', async (req, res) => {
-  const client = new MongoClient(db.data.connectionURL);
-  try {
-    await client.connect();
-    let dbs = client.db(db.data.databaseName);
-    dbs.listCollections().toArray(function (err, collections) {
-      res.render('index', { list: collections });
-      //collections = [{"name": "coll1"}, {"name": "coll2"}]
-    });
-  } catch (err) {
-    console.error(err);
-  } finally {
-    await client.close();
-  }
+	const client = new MongoClient(db.data.connectionURL);
+	try {
+		await client.connect();
+		let dbs = client.db(db.data.databaseName);
+		dbs.listCollections().toArray(function (err, collections) {
+			res.render('index', { list: collections });
+			//collections = [{"name": "coll1"}, {"name": "coll2"}]
+		});
+	} catch (err) {
+		console.error(err);
+	} finally {
+		await client.close();
+	}
 });
 
 app.get('/download/:file', (req, res) => {
-  console.log(req.params.file)
-  let localPath = '/home/download' + req.params.file;
-  if (!fs.existsSync('./CSV/' + req.params.file)) {
-    res.status(404).json({ message: 'file not found' });
-    return;
-  }
-  res.download('./CSV/' + req.params.file, localPath);
+	// console.log(req.params.file);
+	//downloadCSV(req.params.file);
+	let localPath = '/home/download' + req.params.file;
+	if (!fs.existsSync('./CSV/' + req.params.file)) {
+		res.status(404).json({ message: 'file not found' });
+		return;
+	}
+	res.download('./CSV/' + req.params.file, localPath);
 });
 
 app.post(
-  '/stock-names',
-  upload.single('stockTickerNames'),
-  async (req, res) => {
-    let avail = false;
-    const client1 = new MongoClient(db.data.connectionURL);
-    try {
-      let arr = await getArrPromise(req.file.path);
-      await client1.connect();
-      let dbs = client1.db(db.data.databaseName);
-      dbs.listCollections().toArray(function (err, collections) {
-        for(let i =0;i<collections.length;i++){
-          if(collections[i].name === req.file.filename){
-            avail = true;
-            return db.connectAndOperateOnDb(arr, updateDb, req.file.filename);
-          }
-        }
-        if(!avail){
-          db.connectAndOperateOnDb(arr, buildAndPopulateDatabase, req.file.filename)
-        }
+	'/stock-names',
+	upload.single('stockTickerNames'),
+	async (req, res) => {
+		let avail = false;
+		const client1 = new MongoClient(db.data.connectionURL);
+		try {
+			let arr = await getArrPromise(req.file.path);
+			await client1.connect();
+			let dbs = client1.db(db.data.databaseName);
+			dbs.listCollections().toArray(function (err, collections) {
+				for (let i = 0; i < collections.length; i++) {
+					if (collections[i].name === req.file.filename) {
+						avail = true;
+						return db.connectAndOperateOnDb(arr, updateDb, req.file.filename);
+					}
+				}
+				if (!avail) {
+					db.connectAndOperateOnDb(
+						arr,
+						buildAndPopulateDatabase,
+						req.file.filename
+					);
+				}
+			});
 
-      });
-
-      // if (avail) {
-      //   db.connectAndOperateOnDb(arr, updateDb, req.file.filename);
-      // } else {
-      //   db.connectAndOperateOnDb(
-      //     arr,
-      //     buildAndPopulateDatabase,
-      //     req.file.filename
-      //   );
-      // }
-      res.redirect('/');
-    } catch (err) {
-      res.send(err);
-    }
-  }
+			// if (avail) {
+			//   db.connectAndOperateOnDb(arr, updateDb, req.file.filename);
+			// } else {
+			//   db.connectAndOperateOnDb(
+			//     arr,
+			//     buildAndPopulateDatabase,
+			//     req.file.filename
+			//   );
+			// }
+			res.redirect('/');
+		} catch (err) {
+			res.send(err);
+		}
+	}
 );
 
 app.listen(port || 3000, () => {
-  console.log('Server is up on port ' + port);
+	console.log('Server is up on port ' + port);
 });
 
+
 function getArrPromise(filename) {
-  return new Promise((resolve) => {
-    readKeyToArray(filename, (err, arr) => {
-      if (err) rejects(err);
-      resolve(arr);
-    });
-  });
+	return new Promise((resolve) => {
+		readKeyToArray(filename, (err, arr) => {
+			if (err) rejects(err);
+			resolve(arr);
+		});
+	});
 }
 
 function readKeyToArray(filename, callback) {
-  fs.readFile(filename, (err, data) => {
-    if (err) callback(err, undefined);
-    callback(undefined, data.toString().split('\n'));
-  });
+	fs.readFile(filename, (err, data) => {
+		if (err) callback(err, undefined);
+		callback(undefined, data.toString().split('\n'));
+	});
 }
+
+/*
+function test() {
+var formData = new FormData();
+formData.append("stockTickerNames", add);
+console.log("im running");
+
+axios.post('http://localhost:3000/stock-names', formData, {
+    headers: {
+      'Content-Type': 'multipart/form-data'
+    }
+})
+}
+*/
+
+let req = request.post('http://localhost:3000/stock-names', function (err, resp, body) {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log('URL: ' + body);
+  }
+});
+
+let form = req.form();
+form.append('stockTickerNames', fs.createReadStream(add));
